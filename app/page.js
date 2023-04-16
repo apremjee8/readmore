00961e246d67ff89d57d027baee3212d22923863
Akiff Premjee <akiff.premjee@gmail.com>
2023-04-15 21:04:04 -0400

    adding langchain

--- app/page.js
+++ app/page.js
@@ -1,91 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+async function getData() {
+  const headers = new Headers({
+    Authorization: `Token ${process.env.READWISE_TOKEN}`,
+  });
 
-const inter = Inter({ subsets: ['latin'] })
+  const options = {
+    method: "GET",
+    headers: headers,
+  };
 
-export default function Home() {
+  const res = await fetch("https://readwise.io/api/v2/export/", options);
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  console.log(res);
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex flex-col space-y-3 w-full p-2'>
+      {data.results.map((item) => (
+        <div key={item.user_book_id} className='flex flex-col space-y-3'>
+          <div className='text-white bg-slate-600 text-lg p-2'>
+            Title: {item.title}
+          </div>
+          <div className='text-white bg-slate-600 text-med p-2'>
+            Author: {item.author}
+          </div>
+          <div className='flex flex-col break-all drop-shadow space-y-3'>
+            {item.highlights.map((highlight) => (
+              <div key={highlight.id} className='bg-slate-100 p-2'>
+                {highlight.text}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      ))}
+    </div>
+  );
 }
